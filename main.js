ec5020a59da34bdc1c4725474b0fe58b562df2eb
xime-rom912 <45503812+xime-rom912@users.noreply.github.com>
2021-04-28 21:04:25 -0600

    Corregi errores en las salidas que solicita

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const Script  = require('./DAO');
 let productosDAO = new Script('./productos.json');
 
 console.log('1) Número de productos con existencia mayor a 20.');
-console.log(productosDAO.productsInStockMore(20));
+console.log(productosDAO.productsInStockMore(20).length);
 
 console.log('2) Número de productos con existencia menos a 15.');
-console.log(productosDAO.productsInStockLess(15));
+console.log(productosDAO.productsInStockLess(15).length);
 
 console.log('3) Lista de productos con la misma clasificación y precio mayor 15.50');
 console.log(productosDAO.productsSameCategoryPriceMore(15.5,'verdura'));
@@ -15,4 +15,4 @@ console.log('4) Lista de productos con precio mayor a 20.30 y menor a 45.00');
 console.log(productosDAO.productsPriceBetween(20.3,45));
 
 console.log('5) Número de productos agrupados por su clasificación');
-console.log(productosDAO.productsSameCategory('utensilios de cocina'));
+console.log(productosDAO.productsSameCategory('utensilios de cocina').length);
